e2817950e2012c7566eb8d7038244e70deac1e6b
FilippovaLena <lenchf96@gmail.com>
2025-02-18 10:56:19 +0300

    Update 02.js

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,14 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const result = [];
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    const transformedElement = callback(element, index);
+    result.push(transformedElement);
+}
+
+  return result;
+};

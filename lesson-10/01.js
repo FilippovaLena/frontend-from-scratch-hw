5e036b7c50c1d1e0f694a4087c1dbaa0bc7a63d0
FilippovaLena <lenchf96@gmail.com>
2025-02-20 20:41:41 +0300

    Update 01.js

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -22,161 +22,92 @@
 */
 
 const model = {
-
   movies: [],
-
   addMovie(title, description) {
-
     const id = Math.random();
-
     const newMovie = { id, title, description };
-
     this.movies.push(newMovie);
-
     view.renderMovies(this.movies);
-
   },
-
   deleteMovie(id) {
-
     this.movies = this.movies.filter(movie => movie.id !== id);
-
     view.renderMovies(this.movies);
-
   }
-
-};
-
-const view = {
-
+ };
+ 
+ const view = {
   init() {
-
     this.renderMovies(model.movies);
-
+ 
     const form = document.querySelector('.form');
-
     const inputTitle = document.querySelector('.input-title');
-
     const inputDescription = document.querySelector('.input-description');
-
+ 
     form.addEventListener('submit', function (event) {
-
       event.preventDefault();
-
       const title = inputTitle.value;
-
       const description = inputDescription.value;
-
       controller.addMovie(title, description);
-
+ 
       inputTitle.value = '';
-
       inputDescription.value = '';
-
     });
-
+ 
     const list = document.querySelector('.list');
-
-    list.addEventListener('click', (event) => {
-
+    list.addEventListener('click', function(event) {
       if (event.target.classList.contains('delete-button')) {
-
-        const movieId = event.target.parentNode.id;
-
-        controller.deleteMovie(movieId);
-
+        const movieItem = event.target.closest('.movie');
+        const id = movieItem.id;
+        controller.deleteMovie(id);
       }
-
     });
-
   },
-
   renderMovies(movies) {
-
     const list = document.querySelector('.list');
-
     let moviesHTML = '';
-
+ 
     for (const movie of movies) {
-
       moviesHTML += `
-
         <li id="${movie.id}" class="movie">
-
           <b class="movie-title">${movie.title}</b>
-
           <p class="movie-description">${movie.description}</p>
-
-          <button class="delete-button" type="button">Удалить 🗑</button>
-
+          <button class="delete-button" type="button">Удалить</button>
         </li>
-
       `;
-
     }
-
+ 
     list.innerHTML = moviesHTML;
-
   },
-
   displayMessage(message, isError = false) {
-
     const messageBox = document.querySelector('.message-box');
-
     messageBox.textContent = message;
-
     if (isError) {
-
       messageBox.classList.remove('success');
-
       messageBox.classList.add('error');
-
     } else {
-
       messageBox.classList.remove('error');
-
       messageBox.classList.add('success');
-
     }
-
   },
-
-};
-
-const controller = {
-
+ };
+ 
+ const controller = {
   addMovie(title, description) {
-
     if (title.trim() !== '' && description.trim() !== '') {
-
       model.addMovie(title, description);
-
       view.displayMessage('Фильм добавлен успешно!');
-
     } else {
-
       view.displayMessage('Заполните все поля!', true);
-
     }
-
   },
-
   deleteMovie(id) {
-
     model.deleteMovie(id);
-
     view.displayMessage('Фильм успешно удалён!');
-
   }
-
-};
-
-function init() {
-
+ };
+ 
+ function init() {
   view.init();
-
-}
-
-document.addEventListener('DOMContentLoaded', init);
-
-
+ }
+ 
+ document.addEventListener('DOMContentLoaded', init);

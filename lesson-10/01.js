cae84fa77822a7106bcf91da925a8465290f0321
FilippovaLena <lenchf96@gmail.com>
2025-02-20 20:11:14 +0300

    Update 01.js

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -30,7 +30,7 @@ const model = {
     view.renderMovies(this.movies);
   },
   deleteMovie(id) {
-    this.movies = this.movies.filter((movies) => movie.id !== id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
     view.renderMovies(this.movies);
   }
  };
